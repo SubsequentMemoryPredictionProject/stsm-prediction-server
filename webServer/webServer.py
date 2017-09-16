8175351ae5a50509026b6a825dd8e4f8dc597b70
gal <gal.peretz@zeek.me>
2017-09-16 12:48:50 +0300

    integration test

--- webServer/webServer.py
+++ webServer/webServer.py
@@ -11,6 +11,7 @@ def test():
     return json.dumps({'ok': True})
 
 if __name__ == '__main__':
-    logger = Logger().get_logger()
-    logger.info('Starting web server...')
+    # logger = Logger().get_logger()
+    # logger.info('Starting web server...')
+    print('Starting web server...')
     app.run(host='0.0.0.0', port=3100)
