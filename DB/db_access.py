0b605824b1492051e3ce7ee86af70f2d23a2a668
rotemjordan <rotemjordan@gmail.com>
2017-09-20 13:13:47 +0300

    Merge branch 'master' of https://github.com/SubsequentMemoryPredictionProject/stsm-prediction-server
    # Conflicts:
    #	predictionModel/load_model.py
    #	webServer/webServer.py

--- DB/db_access.py
+++ DB/db_access.py
@@ -31,10 +31,10 @@ def get_signals(db):
     word = []
     query1 = 'SELECT signal_elec1_subelec1, signal_elec1_subelec2, \
              signal_elec1_subelec3, signal_elec2_subelec1, signal_elec2_subelec2, \
-             signal_elec2_subelec3 from data_set WHERE EEG_data_section=1 '
+             signal_elec2_subelec3 from data_set WHERE EEG_data_section=1 LIMIT 0,4000'
     query2 = 'SELECT signal_elec3_subelec1, signal_elec3_subelec2, \
              signal_elec3_subelec3, signal_elec4_subelec1, signal_elec4_subelec2, \
-             signal_elec4_subelec3 FROM data_set WHERE EEG_data_section=2 '
+             signal_elec4_subelec3 FROM data_set WHERE EEG_data_section=2 LIMIT 0,4000'
     section_one = get_data(db, query1)
     print("got section one")
     print(len(section_one))
@@ -77,7 +77,7 @@ def get_results(db):
     print('in get results')
     results = []
     query = 'SELECT stm, stm_confidence_level, stm_remember_know, ltm, \
-             ltm_confidence_level, ltm_remember_know FROM data_set WHERE EEG_data_section=1 '
+             ltm_confidence_level, ltm_remember_know FROM data_set WHERE EEG_data_section=1 LIMIT 0,4000'
     data_set = get_data(db, query)
     for row in data_set:
         # ignore missing words
