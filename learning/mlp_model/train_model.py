695b7f317196650989e6f5193f199d656733b1ef
rotemjordan <rotemjordan@gmail.com>
2017-09-23 13:35:23 +0300

    Merge branch 'master' of https://github.com/SubsequentMemoryPredictionProject/stsm-prediction-server
    # Conflicts:
    #	predictionModel/load_model.py
    #	webServer/webServer.py

--- learning/mlp_model/train_model.py
+++ learning/mlp_model/train_model.py
@@ -23,7 +23,7 @@ try:
     conn = pymysql.connect(host=cfg.mysql['host'], passwd=cfg.mysql['password']
                      , port=cfg.mysql['port'], user=cfg.mysql['user'], db=cfg.mysql['database'])
 
-    mlp_model = MLPClassifier(max_iter=400)
+    classifiers = [MLPClassifier(max_iter=400), MLPClassifier(max_iter=400,hidden_layer_sizes=(120,120,120,120))]
 
     # load data to train & test model
     X = get_signals(conn)
@@ -39,14 +39,14 @@ try:
     # split data to training and testing set
     X_train, X_test, \
         Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
-    multi_mlp_model = MultiOutputClassifier(mlp_model, n_jobs=1)
-    multi_mlp_model.fit(X_train, Y_train)
-    print('finished model fit')
-    predictions = multi_mlp_model.predict(X_test)
+    for clf in classifiers:
+        multi_mlp_model = MultiOutputClassifier(clf, n_jobs=1)
+        multi_mlp_model.fit(X_train, Y_train)
+        print('finished model fit')
 
-    evaluate_model(multi_mlp_model,X_test,Y_test)
-    # save trained model
-    joblib.dump(multi_mlp_model, 'mlp_model.pkl')
+        evaluate_model(multi_mlp_model,X_test,Y_test)
+        # save trained model
+        #joblib.dump(multi_mlp_model, 'mlp_model.pkl')
 except:
     print(sys.exc_info()[0])
     raise
