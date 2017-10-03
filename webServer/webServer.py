7b96d4687cf469e1f54fb12546c993c9dfb9eefc
gal <gal.peretz@zeek.me>
2017-10-03 12:46:02 +0300

    init bug

--- webServer/webServer.py
+++ webServer/webServer.py
@@ -22,7 +22,8 @@ stsm_model = None
 @app.route('/stsm/algorithms/predict/', methods=['POST'])
 def predict():
     try:
-        print('request.form', request.form["1"])
+        print('request.form', request.form())
+        print('request.form', request.get_json())
 
         # stsm_model.evaluate(request.get_json())
         print('returning Success=True')
