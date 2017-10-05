dee3af498361f44c62a0764cb0a5e6bafb5a04aa
rotemjordan <rotemjordan@gmail.com>
2017-10-05 11:50:51 +0300

    Merge branch 'master' of https://github.com/SubsequentMemoryPredictionProject/stsm-prediction-server
    # Conflicts:
    #	predictionModel/load_model.py
    #	webServer/webServer.py

--- DB/db_access.py
+++ DB/db_access.py
@@ -46,8 +46,6 @@ def get_signals(db, user_query='',table='data_set'):
     print("got section 2")
     print(np.shape(section_one))
     print(np.shape(section_two))
-    print(section_one)
-    print(section_two)
     for i in range(len(section_one)):
         print("word  = ",i)
         for j in range(NUM_ELECTRODES):
