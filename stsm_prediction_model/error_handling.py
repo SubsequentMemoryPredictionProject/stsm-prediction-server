7cce69cedfedbcb11b717baef0656b850fc7d706
rotemjordan <rotemjordan@gmail.com>
2017-10-07 14:26:57 +0300

    Merge branch 'master' of https://github.com/SubsequentMemoryPredictionProject/stsm-prediction-server
    # Conflicts:
    #	predictionModel/load_model.py
    #	webServer/webServer.py

--- stsm_prediction_model/error_handling.py
+++ stsm_prediction_model/error_handling.py
@@ -1,10 +1,10 @@
-
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-
-class LoadModelError(Error):
-    def __init__(self, msg):
-        Error.__init__(self)
-        self.message: msg
+#
+# class Error(Exception):
+#     """Base class for exceptions in this module."""
+#     pass
+#
+#
+# class LoadModelError(Error):
+#     def __init__(self, msg):
+#         Error.__init__(self)
+#         self.message: msg
