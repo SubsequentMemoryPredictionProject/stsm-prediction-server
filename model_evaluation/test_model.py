491d9a033a4f64677633a7faf6c0d5389cf23810
rotemjordan <rotemjordan@gmail.com>
2017-10-13 12:22:22 +0300

    Merge branch 'master' of https://github.com/SubsequentMemoryPredictionProject/stsm-prediction-server
    # Conflicts:
    #	predictionModel/load_model.py
    #	webServer/webServer.py

--- model_evaluation/test_model.py
+++ model_evaluation/test_model.py
@@ -1,7 +1,6 @@
 from sklearn import metrics
-from stsm_prediction_model.error_handling import ModelError
 from sklearn.externals import joblib
-from model_evaluation.cross_validation import d_prime
+e
 import numpy as np
 import pymysql.connections
 import sys
@@ -9,6 +8,9 @@ import os
 NUM_RESULTS = 6
 PROJECT_ROOT = os.path.abspath('.')
 sys.path.append(PROJECT_ROOT)
+from stsm_prediction_model.error_handling import ModelError
+
+from model_evaluation.cross_validation import d_prim
 import config as cfg
 from logger import Logger
 from DB.db_access import choose_signals
