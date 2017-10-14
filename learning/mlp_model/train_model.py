703bd86b57e3639d0e6a83db7bbda3e37c5bf935
rotemjordan <rotemjordan@gmail.com>
2017-10-14 18:41:38 +0300

    Merge branch 'master' of https://github.com/SubsequentMemoryPredictionProject/stsm-prediction-server
    # Conflicts:
    #	predictionModel/load_model.py
    #	webServer/webServer.py

--- learning/mlp_model/train_model.py
+++ learning/mlp_model/train_model.py
@@ -25,7 +25,7 @@ def train_and_save(db, electrode, duration, layer=cfg.mlp_params['layers'], acti
                    cross_val=5, learning_rate=cfg.mlp_params['learning_rate']):
     try:
         mlp_model = MLPClassifier(verbose=True, hidden_layer_sizes=layer, activation=activation,
-                                  solver='sgd', learning_rate=learning_rate, max_iter=500)
+                                  solver='sgd', learning_rate=learning_rate, max_iter=100)
         multi_mlp_model = MultiOutputClassifier(mlp_model, n_jobs=1)
     except:
         raise ModelError('Error in creating MLP model', 4001, str(sys.exc_info()))
